d49ff203e6c9552130d2ad4c279ad4051652f209
Ari Sabah <arisabah2001@gmail.com>
2024-04-13 19:51:50 +0300

    Update package.json and mysqlAsyncPoolConnection.js

--- scripts/mysqlAsyncPoolConnection.js
+++ scripts/mysqlAsyncPoolConnection.js
@@ -1,50 +1,51 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 
-dotenv.config({ path: './.env' })
+dotenv.config({ path: "./.env" });
 
-const pool  = mysql.createPool({
-  connectionLimit : 10,
-  host     : process.env.DATABASE_HOST,
-  user     : process.env.DATABASE_USER,
-  password : process.env.DATABASE_PASSWORD,
-  database : process.env.DATABASE,
-  ssl: {
-    ca: '/etc/ssl/certs/ca-certificates.crt',
-    rejectUnauthorized: false
-  }
+const pool = mysql.createPool({
+	connectionLimit: 10,
+	host: process.env.DATABASE_HOST,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE,
+	port: process.env.DATABASE_PORT,
+	ssl: {
+		ca: "/etc/ssl/certs/ca-certificates.crt",
+		rejectUnauthorized: false,
+	},
 });
 
 const database = {
-    query: (query, data = []) => {
-      const promise = new Promise((resolve, reject) => { 
-        pool.getConnection((err, connection) => {
-          if(err) {
-            reject(err);
-          }
+	query: (query, data = []) => {
+		const promise = new Promise((resolve, reject) => {
+			pool.getConnection((err, connection) => {
+				if (err) {
+					reject(err);
+				}
 
-          connection.query(query, data, (err, data, fields) => {
-            connection.release();
-            
-            if(err) {
-              reject(err);
-            }
-            
-            resolve(data, fields);
-          });
-        });
-      });
+				connection.query(query, data, (err, data, fields) => {
+					connection.release();
 
-      return promise
-        .then((data, fields) => {
-          return {data, fields};
-        })
-        .catch((err) => {
-          throw new Error(err);
-        })
-    },
-    escape: (data) => pool.escape(data),
-    getDatabaseName: () => process.env.DATABASE
-}
+					if (err) {
+						reject(err);
+					}
+
+					resolve(data, fields);
+				});
+			});
+		});
+
+		return promise
+			.then((data, fields) => {
+				return { data, fields };
+			})
+			.catch((err) => {
+				throw new Error(err);
+			});
+	},
+	escape: (data) => pool.escape(data),
+	getDatabaseName: () => process.env.DATABASE,
+};
 
 module.exports = database;
